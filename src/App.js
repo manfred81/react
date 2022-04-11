b6e4d05e590a28140cc9da44f3afa8bd0c919aa1
manfred81 <igor_travkin@mail.ru>
2022-04-11 20:13:18 +0300

    20_15

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.scss';
 import Router from './pages/Router';
+import React, {useState} from 'react';
+
+
+export const MyThemeContext = React.createContext({ theme: 'dark'});
+
 
 function App() { 
+  const [theme, setTheme] = useState('dark')
     return (
       <div className="App">
-        <header className= "App-header"> 
+        <header className= "App-header">
 
-        <Router/>
+        <MyThemeContext.Provider value={{theme: theme, setTheme: setTheme }}>
+          <Router/>
+        </MyThemeContext.Provider>
       
       </header>
      
