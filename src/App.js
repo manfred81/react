5b7120c4bc3809a831d9be8c195ca6db26c92e75
manfred81 <igor_travkin@mail.ru>
2022-04-11 13:30:09 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 
 import './App.scss';
 import './pages/Router';
-<<<<<<< HEAD
-
-=======
-import { Router } from 'react-router-dom';
->>>>>>> f8f0889e98b2cbadbdbca9d26c5929d9dbdb4231
-
-
-
 
 function App() { 
     return (
       <div className="App">
         <header className= "App-header"> 
-<<<<<<< HEAD
+
         <Router/>
-=======
-        <Router />
->>>>>>> f8f0889e98b2cbadbdbca9d26c5929d9dbdb4231
-       
+      
       </header>
      
     </div>
