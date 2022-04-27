f676d5b438ed62845f0e18d1c3f9105dd9eb41ed
manfred81 <igor_travkin@mail.ru>
2022-04-27 22:52:33 +0300

    dz9

--- src/Pages/Gists.js
+++ src/Pages/Gists.js
@@ -1,29 +1,28 @@
 import { CircularProgress } from '@mui/material';
-import { useEffect, useCallback, useState } from 'react';
+import { useEffect, useCallback} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllGists } from '../store/gists/actions';
 import { selectGists, selectGistsError, selectGistsLoading } from '../store/gists/selectors';
 
-const maxAmount = 3;
+
 
 const Gists = () => {
    const dispatch = useDispatch();
    const gists = useSelector(selectGists);
    const loading = useSelector(selectGistsLoading);
    const error = useSelector(selectGistsError);
-   const [count, setCount] = useState(0);
+ 
 
     const reguestGists =  async () => {
             dispatch(getAllGists());
       }; 
 
-    useEffect(()=> {
+    useEffect(() => {
         reguestGists();
     }, []);
 
     const renderGist = useCallback(
-        (gist)=> 
-    <li key={gist.id}>{gist.description || 'No description'}</li>,
+        (gist) => <li key={gist.id}>{gist.description || 'No description'}</li>,
     []
     );
 
